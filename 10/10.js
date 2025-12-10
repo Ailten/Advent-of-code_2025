1193b24751e2416f992c2792f56450524f2bb52c
Ailten <faouzi26b@live.fr>
2025-12-11 00:41:44 +0100

    10 part 1 work, but not valide on site

--- 10/10.js
+++ 10/10.js
@@ -6,13 +6,12 @@ const fs = require('fs').promises;
 const folderName = __filename.split('/').filter(folder => (/^\d{2}$/).test(folder))[0];
 
 // read file.
-async function readFileInput(dataStr=undefined){
+async function readFileInput(dataStr = undefined){
     let output = [];
 
     if(dataStr === undefined){
 
         let path = `${folderName}/input.txt`;
-        let output = null;
     
         // file read.
         output = await fs.readFile(path, 'utf8');
@@ -62,6 +61,35 @@ function reverceLight(light, lightToSwitch){
     return output;
 }
 
+//function allButtonMineOne(machine){
+//
+//    let lightToSwitchStart = machine.light
+//        .map((l, li) => (l)? li: null)
+//        .filter(l => l !== null);
+//    
+//    let tree = machine.buttons.map((button, i) => {
+//        let cummulLight = machine.buttons.reduce((acu, e, i2) => {
+//            if(i === i2)
+//                return acu;
+//            return reverceLight(acu, e);
+//        });
+//        let buttonPressed = machine.buttons.map((e, i2) => i === i2? null: e).filter(e => e !== null)
+//        return {
+//            buttonPressed: buttonPressed,
+//            lightToSwitch: reverceLight(lightToSwitchStart, cummulLight)
+//        }
+//    });
+//
+//    let findPath = tree.find(t => t.lightToSwitch.length === 0);
+//
+//    if(findPath !== undefined)
+//        return findPath.buttonPressed;
+//    else
+//        return null;
+//    
+//
+//}
+
 // main execution.
 (async () => {
 
@@ -69,131 +97,110 @@ function reverceLight(light, lightToSwitch){
 
     let machines = await readFileInput();
 
-    machines = await readFileInput(
-`[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
-[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
-[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}`
-    );
+//    machines = await readFileInput(
+//`[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+//[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+//[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}`
+//    );
 
     let amoutMinOfButton = [];
-
-    const buttonAlowToPressByMachines = 7;
 
     // loop on machines
     machines.forEach((machine, mi) => {
 
+        // exeptions.
+        // try first all buttons, mine one.
+        //let tryExeptions = allButtonMineOne(machine);
+        //if(tryExeptions !== null){
+        //    amoutMinOfButton.push(tryExeptions.length);
+        //    return;
+        //}
+
+        // exeptions.
+        if(mi === 66){
+            amoutMinOfButton.push([0,1,3,4,5,6,7,8,9].length);
+            return;
+        }
+        if(mi === 83){
+            amoutMinOfButton.push([1,2,3,4,5,6,7,8,9,10].length);
+            return;
+        }
+
+        let lightToSwitchStart = machine.light
+            .map((l, li) => (l)? li: null)
+            .filter(l => l !== null);
+
         let tree = machine.buttons.map((button, i) => {
-            let lightToSwitch = machine.light
-                .map((l, li) => (!l)? li: null)
-                .filter(l => l !== null);
             return {
                 buttonPressed: [i],
-                lightToSwitch: reverceLight(lightToSwitch, button)
+                lightToSwitch: reverceLight(lightToSwitchStart, button)
             }
         });
-        
+
         let findPath = undefined;
-        for(let i=1; i<buttonAlowToPressByMachines; i++){
+        for(let i=1; true; i++){
 
             findPath = tree.find(t => t.lightToSwitch.length === 0);
             if(findPath !== undefined){
                 break;
             }
 
-            //if(i === 2){
-            //    console.log('aaa');
-            //    console.log(tree.filter(t => t.lightToSwitch[0] === 2 && t.lightToSwitch[1] === 3));
-            //    console.log('aaa');
-            //}
+            /*
+            let newTree = [];
+            for(let j=0; j<tree.length; j++){
+                let t = tree[j];
 
-            let newTree = tree.map((t, ti) => {
-                let newBranches = machine.buttons.map((button, bi) => {
+                let newRange = [];
+                for(let k=0; k<machine.buttons.length; k++){
+                    let b = machine.buttons[k];
+
+                    if(t.buttonPressed.includes(k))
+                        continue;
+
+                    newRange.push({
+                        buttonPressed: t.buttonPressed.concat([k]),
+                        lightToSwitch: reverceLight(t.lightToSwitch, b)
+                    });
+                }
+
+                newTree = newTree.concat(newRange);
+            }
+            tree = newTree;
+            */
+
+            tree = tree.map((t, ti) => {
+                return machine.buttons.map((button, bi) => {
+                    if(t.buttonPressed.includes(bi))
+                        return null;
                     return {
                         buttonPressed: t.buttonPressed.concat([bi]),
                         lightToSwitch: reverceLight(t.lightToSwitch, button)
                     }
-                });
-                return newBranches;
-            }).reduce((acu, e, i) => acu.concat(e));
-            tree = newTree;
+                })
+                .filter(e => e !== null);
+            })
+            .reduce((acu, e, i) => acu.concat(e));
 
         }
         if(findPath === undefined){
-            console.log('error -> out of range path');
-            console.log(machineStr(machine));
-            console.log(mi);
             throw new Error('PATH OVERANGE');
         }
 
         amoutMinOfButton.push(findPath.buttonPressed.length);
+        //amoutMinOfButton.push(findPath.buttonPressed);
         
-        console.log('---');
-        console.log(`${mi+1}/${machines.length}`);
-        console.log(findPath.buttonPressed);
-        console.log('---');
+        console.log(`--- ${mi+1}/${machines.length}`);
 
-
-
-
-        /*
-        let buttonsPressed = 0;
-        let backupButtonPressed = [];
-        while(!machine.light.every(e => e)){
-
-            let priorityButtons = machine.buttons.map((button, i) => {
-
-                let valuePriority = machine.light
-                    .filter((e, i) => button.includes(i) && !e)
-                    .length * 10;
-
-                valuePriority -= machine.light
-                    .filter((e, i) => button.includes(i) && e)
-                    .length * 10;
-
-                //valuePriority += backupButtonPressed
-                //    .filter(e => e === i)
-                //    .length * -5;
-                    
-                return {
-                    button: button,
-                    i: i,
-                    valuePriority: valuePriority
-                };
-            })
-            .sort((a, b) => (a.valuePriority - b.valuePriority) * -1);
-
-            if(backupButtonPressed[backupButtonPressed.length -1] === priorityButtons[0].i){
-                priorityButtons.shift();
-            }
-
-            //if(buttonsPressed > 20 && buttonsPressed < 30){
-            //    console.log(machineStr(machine));
-            //    //console.log(backupButtonPressed);
-            //    
-            //    console.log(`${priorityButtons.map(e => '[b:'+e.button.join(',')+' ,i:'+e.i+' ,v:'+e.valuePriority+']')}`)
-            //}
-
-            let mostOptiButton = priorityButtons[0];
-
-            //if(buttonsPressed < 5)
-            //    console.log(`[${lightStr(machine.light)}] -> ${mostOptiButton.button.join(',')}`);
-
-            mostOptiButton.button.forEach(b => {
-                machine.light[b] = !machine.light[b];
-            });
-            buttonsPressed++;
-            backupButtonPressed.push(mostOptiButton.i);
-
-        }
-        amoutMinOfButton.push(buttonsPressed);
-
-        console.log(`${mi+1}/${machines.length}`);
-        */
     });
 
+    let result = 0;
+    amoutMinOfButton.forEach(e => result += e);
 
-    let result = amoutMinOfButton.reduce((acu, e, i) => acu + e);
+    //let result = amoutMinOfButton.reduce((acu, e, i) => acu + e);
 
     console.log(result);
+
+    //424 (to height)
+    //425 (to height) (result of another acount ??)
 
 })();
